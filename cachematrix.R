dcfbc4caa4011176d38d94f4f8eb9318659d5c69
tanisha14082002parkash <79708114+tanisha14082002parkash@users.noreply.github.com>
2022-11-01 16:17:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -38,8 +38,8 @@ cacheSolve <- function(x, ...) {
 }
 
 #' Compute and cache the inverse of a matrix
-#' @param x the result of a previous makeCacheMatrix call
-#' @param ... additional arguments to pass to solve function
+#' param x the result of a previous makeCacheMatrix call
+#' param ... additional arguments to pass to solve function
 #' examples
 #' x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
 #' cacheSolve(x)
